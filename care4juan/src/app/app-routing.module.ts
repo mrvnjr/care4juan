2584b66c192bcb3a02561c62c53c050277dd1341
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-09-13 23:24:19 +0800

    soloparent

--- care4juan/src/app/app-routing.module.ts
+++ care4juan/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BarangaydashComponent } from './barangaydash/barangaydash.component';
-
+import { SoloparentComponent } from './soloparent/soloparent.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'barangaydash', component: BarangaydashComponent },
-  { path: '', redirectTo:'/login', pathMatch: 'full'},
+  { path: 'soloparent', component: SoloparentComponent },
+  { path: '', redirectTo:'/barangaydash', pathMatch: 'full'},
 ];
 
 @NgModule({
--- care4juan/src/app/app.module.ts
+++ care4juan/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BarangaydashComponent } from './barangaydash/barangaydash.component';
+import { SoloparentComponent } from './soloparent/soloparent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BarangaydashComponent
+    BarangaydashComponent,
+    SoloparentComponent
   ],
   imports: [
     BrowserModule,
