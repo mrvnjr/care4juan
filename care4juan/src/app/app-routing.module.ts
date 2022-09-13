1ca832c04537a9d1bfe0a894499da872a4453344
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-09-13 22:45:20 +0800

    design

--- care4juan/src/app/app-routing.module.ts
+++ care4juan/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { BarangaydashComponent } from './barangaydash/barangaydash.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'barangaydash', component: BarangaydashComponent },
   { path: '', redirectTo:'/login', pathMatch: 'full'},
 ];
 
--- care4juan/src/app/app.module.ts
+++ care4juan/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BarangaydashComponent } from './barangaydash/barangaydash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BarangaydashComponent
   ],
   imports: [
     BrowserModule,
