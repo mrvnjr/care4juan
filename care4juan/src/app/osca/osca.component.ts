e57690762c92e77af7e6ef0159f18b3ea60ccb24
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-09-15 09:39:49 +0800

    Login directiory

---
+++ care4juan/src/app/osca/osca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-osca',
+  templateUrl: './osca.component.html',
+  styleUrls: ['./osca.component.css']
+})
+export class OscaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
